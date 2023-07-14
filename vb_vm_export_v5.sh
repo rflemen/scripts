c16d807dfc7847a80b7872ff3ce85848c6486211
rflemen <rflemen@gmail.com>
2023-07-14 11:01:43 -0400

    mod

--- vb_vm_export_v5.sh
+++ vb_vm_export_v5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # EXPORT .OVA OF VIRTUALBOX VIRTUAL MACHINE v5
-# Author: Rob Flemen    Email: rflemen@gmail.com
+# Author: Rob Flemen    Email: rob@marleylilly.com
 # REQUIRES: ssmtp package. Need systemctl and ssmtp set for sudoers with no password.
 
 # Global variables
@@ -9,14 +9,14 @@ vm_name="snipeit"
 time_stamp=$(date +%m-%d-%Y-%H-%M-%S)
 backup_path="/home/mladmin/Backups/snipeit"
 log_file_name="/home/mladmin/Backups/logs/${time_stamp}_backup.log"
-email_receipient="rflemen@gmail.com"
+email_receipient="rob@marleylilly.com"
 
-# Funtion to send email. Args: log file name, email address
+# Funtion to send email. Args: (1)log file name, (2)email address
 function email_log () {
         cat $1 | sudo ssmtp $2
 }
 
-# Check if a command executed. Args: result code, log file name, email address
+# Check if a command executed. Args: (1)result code, (2)log file name, (3)email address
 function check_for_success () {
         if [ $1 == 0 ]; then
                 echo "SUCCESS! Command successfully executed!" >> $2
@@ -27,13 +27,13 @@ function check_for_success () {
         fi
 }
 
-# Verify service state. Args: service, log file, email address
+# Verify service state. Args: (1)service, (2)log file, (3)email address, (4) process ID
 function verify_service_state () {
-        current_status=$(systemctl status $1 | grep -oe running -oe failed)
+        current_status=$(systemctl status $1 | grep -oe running -oe failed -oe dead)
         if [[ "$current_status" == "running" ]]; then
-                echo "${1} is most definitely UP and ${current_status}!!!" >> $2
-        elif  [[ "$current_status" == "failed" ]]; then
-                echo "${1} is most definitely DOWN and stopped!!!" >> $2
+                echo "${service_name} process ID: ${4} is ACTIVE." >> $2
+        elif  [[ "$current_status" == "failed" || "$current_status" == "dead" ]]; then
+                echo "${service_name} process ID: ${4} is DEAD" >> $2
         else
                 echo "Well shoot, something went HORRIBLY wrong!!!" >> $2
                 email_log $2 $3
@@ -54,10 +54,11 @@ check_for_success $? "${log_file_name}" "${email_receipient}"
 
 # Stop the service & verify state
 echo >> "${log_file_name}"
-echo "---Stopping ${service_name}---" >> "${log_file_name}"
+process_id=$(ps -ef | grep "[V]BoxHeadless -s ${vm_name}" | tr -s ' ' | cut -d ' ' -f2)
+echo "---Stopping process ID: ${process_id} -  ${service_name}---" >> "${log_file_name}"
 sudo systemctl stop "${service_name}" >> "${log_file_name}" 2>&1
 check_for_success $? "${log_file_name}" "${email_receipient}"
-verify_service_state "${service_name}" "${log_file_name}" "${email_receipient}"
+verify_service_state "${service_name}" "${log_file_name}" "${email_receipient}" "${process_id}"
 
 #Export a copy of the vm to a .ova file
 echo >> "${log_file_name}"
@@ -69,8 +70,9 @@ echo >> "${log_file_name}"
 echo "---Starting ${service_name}---" >> "${log_file_name}"
 sudo systemctl start "${service_name}" >> "${log_file_name}" 2>&1
 check_for_success $? "${log_file_name}" "${email_receipient}"
-sleep 10 #give service a chance to completely start
-verify_service_state "${service_name}" "${log_file_name}" "${email_receipient}"
+sleep 5
+process_id=$(ps -ef | grep "[V]BoxHeadless -s ${vm_name}" | tr -s ' ' | cut -d ' ' -f2)
+verify_service_state "${service_name}" "${log_file_name}" "${email_receipient}" "${process_id}"
 
 # Send a copy of the logfile to interested parties
 email_log "${log_file_name}" "${email_receipient}"
